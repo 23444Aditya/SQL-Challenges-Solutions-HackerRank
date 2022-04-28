5082f725ec7d2b85c784c7b90b655296771576af
Aditya Narayan <aditya.kt97@gmail.com>
2022-04-28 22:56:30 +0530

    Solution for HackerRank SQL Challenge - Advanced Select

---
+++ Advanced Select/Binary_Tree_Nodes.sql
@@ -0,0 +1,10 @@
+--@Author='Aditya Narayan'
+-- Binary Tree Nodes
+	SELECT N,
+		CASE 
+			WHEN P IS NULL THEN "Root"
+			WHEN N IN (SELECT P FROM BST) THEN "Inner"
+			ELSE "Leaf"
+		END
+	FROM BST
+	ORDER BY N
